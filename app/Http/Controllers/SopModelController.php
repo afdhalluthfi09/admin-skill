27f45639e4722d330fe75f2740ace0351b594b81
Afdhal Luthfi <afdhal.luthfi@students.amikom.ac.id>
2022-12-12 11:15:40 +0700

    nambahfile

---
+++ app/Http/Controllers/SopModelController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SopModelController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('pages.sop.index');
+    }
+}
---
+++ resources/views/pages/sop/index.blade.php
@@ -0,0 +1,3 @@
+<x-dashboard-layout>
+    <h1>Model Kerja</h1>
+</x-dashboard-layout>
