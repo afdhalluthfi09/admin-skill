335cd326e606765c50a8f4803c4e3890b9134f62
Afdhal Luthfi <afdhal.luthfi@students.amikom.ac.id>
2022-12-24 05:32:44 +0700

    ubah file

--- app/Repositories/Youtube.php
+++ app/Repositories/Youtube.php
@@ -2,42 +2,29 @@
 
 namespace Repositories;
 
+use Illuminate\Support\Facades\Http;
+
 class Youtube
 {
 
 
     public function getListYoutube($request)
     {
-        $curl = curl_init();
+        $response =Http::withHeaders([
+            'Accept'=>'appplication/json',
+        ])
+        ->get('https://www.googleapis.com/youtube/v3/playlistItems',[
+            'part'=>'contentDetails,snippet,status',
+            'playlistId'=>$request,
+            'key'=>'AIzaSyA1dXY9lPe9zi264_4TzJ6D_DQu344xV8k',
+            'maxResults'=>5
 
-        curl_setopt_array($curl, [
-            CURLOPT_URL => "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2Csnippet%2Cstatus&playlistId=".$request."&key=AIzaSyA1dXY9lPe9zi264_4TzJ6D_DQu344xV8k&maxResults=5",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_POSTFIELDS => "",
-            CURLOPT_HTTPHEADER => [
-                "Accept: */*",
-                "User-Agent: Thunder Client (https://www.thunderclient.com)",
-                "content-type: multipart/form-data; boundary=---011000010111000001101001"
-            ],
         ]);
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-           $decode = json_decode($response,true);
-           $items = $decode['items'];
+        if($response->successful()){
+            return $response->collect(['items']);
+        }else{
+            return $response->failed();
         }
-        return $items;
     }
     public function getDetailEvent () {
         $curl = curl_init();
